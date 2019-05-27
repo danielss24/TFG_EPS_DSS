5c8a0f9bde2f1d22a8e000c4d7bfa99be6380edf
dSerena <daniels_s_24@hotmail.com>
2019-05-27 11:26:18 +0200

    UpdatePlot

--- Programa/plotWind.py
+++ Programa/plotWind.py
@@ -2,6 +2,7 @@ import csv
 
 import Pckg.Utils.utils as util
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def getXY(fichero, xIndex=0, yIndex=1, cabeceras = False):
@@ -67,30 +68,73 @@ def secondPlot(fichero):
 
 def thirdPlot(fichero):
 
+    margenPlot = 0.5
+    maxValueData = 90
+
+    #consigue los valores de varianza roll y pitch
     x,y = getXY(fichero,14,15,True)
+    #consigue la fuerza del viento
     z = getDataFromFile(fichero,13,True)
 
     for enum,each in enumerate(z):
-        x[enum] = x[enum] / 90 * each
-        y[enum] = y[enum] / 90 * each
+        x[enum] = x[enum] / maxValueData * each
+        y[enum] = y[enum] / maxValueData * each
 
-    # print(min(x))
-    # print(max(x))
-    # print(min(y))
-    # print(max(y))
+    maxX = max(x) + margenPlot
+    maxY = max(y) + margenPlot
 
+    # x = np.array(x)
+    # y = np.array(y)
+    #
+    # indexX = np.where(x==maxX)
+    # indexY = np.where(y==maxY)
+    #
+    # maxX = x[indexX]
+    # maxY = y[indexY]
+
+    maxAxisValue = 0
+    if maxX > maxY:
+       maxAxisValue = maxX
+    else:
+       maxAxisValue = maxY
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
     plt.scatter(x, y)
-    plt.xlabel('grados')
-    plt.ylabel('grados')
-    #plt.ylim(0, 4)
+    # plt.xlabel('grados')
+    # plt.ylabel('grados')
+    plt.xlim(-maxAxisValue, maxAxisValue)
+    plt.ylim(-maxAxisValue, maxAxisValue)
     # plt.legend()
+    # Move left y-axis and bottim x-axis to centre, passing through (0,0)
+    ax.spines['left'].set_position('center')
+    ax.spines['bottom'].set_position('center')
 
+def fourthPlot(fichero):
+
+    maxValueData = 90
+    # consigue los valores de varianza roll y pitch
+    U, V= getXY(fichero, 14, 15, True)
+    # consigue la fuerza del viento
+    Z = getDataFromFile(fichero, 13, True)
+
+    print(np.sqrt(len(U)))
+
+    X,Y = np.meshgrid(np.arange(0,np.sqrt(len(U)),1),np.arange(0,np.sqrt(len(U)),1))
+
+
+    for enum, each in enumerate(Z):
+        U[enum] = U[enum] / maxValueData * each
+        V[enum] = V[enum] / maxValueData * each
+
+    plt.quiver(X,Y,U,V)
 
 if __name__ == '__main__':
     fichero = util.cargadorFich("./2019/05/07/")
-    firstPlot(fichero)
-    secondPlot(fichero)
-    thirdPlot(fichero)
+    # firstPlot(fichero)
+    # secondPlot(fichero)
+    # thirdPlot(fichero)
+    fourthPlot(fichero)
 
 
     plt.show()
